7d966b5b9978455ccf914724d0d4907d38c7309e
b851TYiytNCk <highpassion90@gmail.com>
2026-06-11 15:46:36 +0200

    Add Direction to export guide

--- layout/layout.php
+++ layout/layout.php
@@ -24,6 +24,26 @@ function get_order_export_layout() {
             background: var(--wooBg);
         }
 
+        .direction-row {
+            border-top: none !important;
+            border-bottom: none !important;
+        }
+
+        .item-direction-block {
+            width: 100%;
+            padding: 0 1.25rem;
+        }
+
+        .item-direction {
+            width: 120px;
+
+            img {
+                display: block;
+                width: 100%;
+                height: auto;
+            }
+        }
+
         @media screen {
             .wooexp-customer,
             .wooexp-body-covered .woocommerce-order-data__heading,
@@ -163,144 +183,37 @@ function get_order_export_layout() {
             return console.error('jQuery is absent. Aborting');
         }
 
-        $(() => {
-            /**
-             * WooCommerce Order item list
-             */
-            var wooItemList = $('#woocommerce-order-items');
-
-            /**
-             * Button that triggers printing
-             */
-            var btn = document.createElement('div');
-            btn.innerText = 'Export Guide';
-            btn.classList.add('button', 'button-primary', 'wooexp-btn');
-
-            if (wooItemList.length) {
-                wooItemList.prepend(btn);
-            }
-
-            btn.addEventListener('click', () => {
-                printElement(wooItemList);
-            })
-
-            var orderItems,
-                imagesProcessed = 0,
-                $body = $('body')
-
-            /**
-             * Print only specific element on a page
-             */
-            function printElement($itemList) {
-                var targetEl = $itemList.clone();
-
-                targetEl.removeClass()
-                    .attr('id', 'woo-item-list')
-                    .appendTo($body)
-
-                if ( !targetEl.length ) {
-                    return console.error('There is no such element on the page.');
+        class Export {
+            constructor() {
+                this.direction_images = 0;
+                this.direction_image_loaded = 0;
+                this.imagesProcessed = 0;
+                this.$body = $('body');
+                this.orderItems = false;
+                this.wooItemList = $('#woocommerce-order-items');
+                this.directions = {
+                    'top': 'top',
+                    'bottom': 'bottom',
+                    'no-preference': 'bottom',
+                    'right': 'right',
+                    'left': 'left'
                 }
-
-                orderItems = targetEl.find('#order_line_items .item');
-
-                /** Prepare DOM elements to clone */
-                const orderNotes = $('[data-name="order_notes_admin"]');
-                const orderNumber = $('.woocommerce-order-data__heading');
-
-                orderItems.each( function(i) {
-                    const $this  = $(this);
-                    const $item_id = $this.data('order_item_id');
-
-                    /** Use to break HTML in order to have each product block undivided on print pages */
-                    $('<tbody class="item-wrap"></tbody>')
-                        .appendTo( $this.closest('.woocommerce_order_items') )
-                        .append($this);
-                    /**
-                     * Create a row that contains item cost and quantity
-                     */
-                    const tr = $('<tr>');
-                    tr.addClass('wooexp-num-row').appendTo($this.parent());
-
-                    $this.find('td.item_cost').appendTo(tr);
-                    $this.find('td.quantity').appendTo(tr);
-
-                    const lineCost = $this.find('td.line_cost');
-                    lineCost
-                        .appendTo(tr)
-                        .find('.refunded, .wc-order-item-discount').remove();
-
-                    /** Insert Order and product number */
-                    const prodName = $this.find('.wc-order-item-name')
-                    const prodParams = new URLSearchParams( '?' + prodName.attr('href').split('?')[1] );
-                    if (prodParams.size) {
-                       $('<div></div>')
-                           .html( `<strong><?php _e( 'Item number', 'wooexp' ); ?>:</strong> #<?php echo get_the_ID()?>-${prodParams.get('post')}`)
-                           .insertAfter( prodName.next() )
-                    }
-
-                    /** Check if current index is the last one to trigger next step and printing */
-                    const isLast = i === orderItems.length - 1;
-
-                    /** Add order notes and order number to each page */
-                    const headerTr = getHeaderTr(orderNumber, orderNotes);
-                    headerTr.prependTo($this.parent());
-
-                    /** Replace thumbnail with artwork original image and place to the right side */
-                    const artwork = $this.find('.download-artwork');
-                    const thumb = $this.find('.thumb')
-                        .appendTo($this);
-                    var imgThumb = thumb
-                        .find('img');
-
-                    if ( ! imgThumb.length ) {
-                        imgThumb = $('<img>');
-                        thumb.find('.wc-order-item-thumbnail').append(imgThumb);
-                    }
-
-                    if ( ! artwork.attr('href') ) {
-                        ++imagesProcessed;
-                        return isLast ? setPrinting(targetEl) : true;
-                    }
-
-                    const imgThumbSrc = imgThumb.attr('src');
-                    var backToSrc;
-
-                    imgThumb
-                        .on('load', function() {
-                            ++imagesProcessed;
-                            imgThumb.removeAttr('width height');
-                            backToSrc && imgThumb.addClass('small-width');
-                            isLast && setPrinting(targetEl);
-                        })
-                        .on('error', function() {
-                            if (imgThumb.attr('src') !== imgThumbSrc) { // If artwork file path is present
-                                if (imgThumbSrc) {
-                                    imgThumb.attr('src', imgThumbSrc);
-                                    backToSrc = true;
-                                } else {
-                                    imgThumb.remove();
-                                    ++imagesProcessed;
-                                    isLast && setPrinting(targetEl);
-                                }
-                            } else if (backToSrc) {
-                                imgThumb.remove()
-                                ++imagesProcessed;
-                                isLast && setPrinting(targetEl);
-                            }
-                        })
-                        .attr('src', artwork.attr('href'));
-                })
+                this.activate()
             }
 
-            function getHeaderTr(orderNumber, orderNotes) {
+            activate() {
+                /** Button that triggers printing */
+                this.addButton(this.wooItemList);
+            }
+
+            getHeaderTr(orderNumber, orderNotes) {
                 const tr = $('<tr>').addClass('wooexp-header-row');
                 const td = $('<td colspan="3">');
 
                 /**
                  * Add order notes
                  */
-                switchTextAreaWithSpan( orderNotes.clone() )
+                this.switchTextAreaWithSpan( orderNotes.clone() )
                     .prependTo(td)
                     .find('.acf-label').text('Order notes: ');
 
@@ -331,60 +244,17 @@ function get_order_export_layout() {
                 return tr.append(td);
             }
 
-            function setPrinting(targetEl) {
+            setPrinting(targetEl) {
+                const self = this;
                 const checkImageLoad = setInterval( function() {
-                    if (orderItems.length === imagesProcessed) {
+                    if (self.orderItems.length === self.imagesProcessed && self.direction_images === self.direction_image_loaded) {
                         clearInterval(checkImageLoad);
-                        startPrinting(targetEl)
+                        self.startPrinting(targetEl)
                     }
                 }, 100);
             }
 
-            function startPrinting(targetEl) {
-                /**
-                 * Clear layout to leave only product data in product section
-                 */
-                targetEl
-                    .find('.postbox-header, thead, .button, p:not(.wrap_note_item, .stamp_equipment, .sales > p, .pet_liner_wrapper), .wc-order-refund-items, #order_line_items')
-                    .remove();
-
-                targetEl.find('.wc-order-totals-items, #order_shipping_line_items, #order_fee_line_items, #order_refunds, .wc-order-add-item, .wc-order-bulk-actions, script')
-                    .remove();
-
-                /**
-                 * Add item notes and Die/Laser file
-                 */
-                targetEl.find('.wrap_note_item, .stamp_equipment').each( function() {
-                    switchTextAreaWithSpan( $(this) );
-                });
-
-                /**
-                 * Attach styles to cloned element
-                 */
-                targetEl.prepend($('#wooexp-style'));
-
-                /**
-                 * Return original HTML contents after successful printing
-                 */
-                $(window).on('afterprint', () => {
-                    location.reload();
-                })
-
-                /**
-                 * Print order items list only
-                 */
-                document.body.classList.add('wooexp-body-covered');
-                try {
-                    $body.children().not('#woo-item-list').remove();
-                } catch (error) {
-                    // TODO: dispose of it
-                    document.querySelector('.mce-panel').remove()
-                }
-
-                window.print();
-            }
-
-            function switchTextAreaWithSpan(el) {
+            switchTextAreaWithSpan(el) {
                 if (el.length) {
                     const textArea = el.find('textarea');
                     const newDetails = $('<span></span>');
@@ -398,6 +268,191 @@ function get_order_export_layout() {
 
                 return el;
             }
+
+            addButton(wooItemList) {
+                var btn = document.createElement('div');
+                btn.innerText = 'Export Guide';
+                btn.classList.add('button', 'button-primary', 'wooexp-btn');
+
+                if (wooItemList.length) {
+                    wooItemList.prepend(btn);
+                }
+
+                console.log(wooItemList);
+
+                btn.addEventListener('click', () => {
+                    this.printElement(wooItemList);
+                })
+            }
+
+            appendDirection(direction, $item) {
+                const $tr = $('<tr>');
+                $tr.addClass('direction-row');
+
+                const $direction_image = $(`<img class="item-direction" src="/wp-content/uploads/wooexp/rewind_images/Rewind_WhiteBopp_Gloss_Rect_${direction}.png" alt="Label direction">`);
+                const $dir_wrap = $('<div class="item-direction-block"></div>');
+
+                $dir_wrap.append($direction_image);
+                $dir_wrap.appendTo($tr);
+                $tr.appendTo($item.parent());
+
+                ++this.direction_images;
+
+                $direction_image.on('load', () => {
+                    ++this.direction_image_loaded;
+                });
+            }
+
+            /**
+             * Print only specific element on a page
+             */
+            printElement($itemList) {
+                var targetEl = $itemList.clone();
+
+                targetEl.removeClass()
+                    .attr('id', 'woo-item-list')
+                    .appendTo(this.$body)
+
+                if ( !targetEl.length ) {
+                    return console.error('There is no such element on the page.');
+                }
+
+                this.orderItems = targetEl.find('#order_line_items .item');
+
+                /** Prepare DOM elements to clone */
+                const orderNotes = $('[data-name="order_notes_admin"]');
+                const orderNumber = $('.woocommerce-order-data__heading');
+
+                const self = this;
+
+                this.orderItems.each( function(i) {
+                    const $this = $(this);
+                    const direction = self.directions[($this.find('#att_pa_direction').text() || '').trim()] || '';
+
+                    /** Use to break HTML in order to have each product block undivided on print pages */
+                    $('<tbody class="item-wrap"></tbody>')
+                        .appendTo( $this.closest('.woocommerce_order_items') )
+                        .append($this);
+
+                    if (direction) {
+                        self.appendDirection(direction, $this);
+                    }
+
+                    /**
+                     * Create a row that contains item cost and quantity
+                     */
+                    const tr = $('<tr>');
+                    tr.addClass('wooexp-num-row').appendTo($this.parent());
+
+                    $this.find('td.item_cost').appendTo(tr);
+                    $this.find('td.quantity').appendTo(tr);
+
+                    const lineCost = $this.find('td.line_cost');
+                    lineCost
+                        .appendTo(tr)
+                        .find('.refunded, .wc-order-item-discount').remove();
+
+                    /** Insert Order and product number */
+                    const prodName = $this.find('.wc-order-item-name')
+                    const prodParams = new URLSearchParams( '?' + prodName.attr('href').split('?')[1] );
+                    if (prodParams.size) {
+                        $('<div></div>')
+                            .html( `<strong><?php _e( 'Item number', 'wooexp' ); ?>:</strong> #<?php echo get_the_ID()?>-${prodParams.get('post')}`)
+                            .insertAfter( prodName.next() )
+                    }
+
+                    /** Check if current index is the last one to trigger next step and printing */
+                    const isLast = i === self.orderItems.length - 1;
+
+                    /** Add order notes and order number to each page */
+                    const headerTr = self.getHeaderTr(orderNumber, orderNotes);
+                    headerTr.prependTo($this.parent());
+
+                    /** Replace thumbnail with artwork original image and place to the right side */
+                    const artwork = $this.find('.download-artwork');
+                    const thumb = $this.find('.thumb')
+                        .appendTo($this);
+                    var imgThumb = thumb
+                        .find('img');
+
+                    if ( ! imgThumb.length ) {
+                        imgThumb = $('<img>');
+                        thumb.find('.wc-order-item-thumbnail').append(imgThumb);
+                    }
+
+                    if ( ! artwork.attr('href') ) {
+                        ++self.imagesProcessed;
+                        return isLast ? self.setPrinting(targetEl) : true;
+                    }
+
+                    const imgThumbSrc = imgThumb.attr('src');
+                    var backToSrc;
+
+                    imgThumb
+                        .on('load', function() {
+                            ++self.imagesProcessed;
+                            imgThumb.removeAttr('width height');
+                            backToSrc && imgThumb.addClass('small-width');
+                            isLast && self.setPrinting(targetEl);
+                        })
+                        .on('error', function() {
+                            if (imgThumb.attr('src') !== imgThumbSrc) { // If artwork file path is present
+                                if (imgThumbSrc) {
+                                    imgThumb.attr('src', imgThumbSrc);
+                                    backToSrc = true;
+                                } else {
+                                    imgThumb.remove();
+                                    ++self.imagesProcessed;
+                                    isLast && self.setPrinting(targetEl);
+                                }
+                            } else if (backToSrc) {
+                                imgThumb.remove()
+                                ++self.imagesProcessed;
+                                isLast && self.setPrinting(targetEl);
+                            }
+                        })
+                        .attr('src', artwork.attr('href'));
+                })
+            }
+
+            startPrinting(targetEl) {
+                const self = this;
+
+                /** Clear layout to leave only product data in product section */
+                targetEl
+                    .find('.postbox-header, thead, .button, p:not(.wrap_note_item, .stamp_equipment, .sales > p, .pet_liner_wrapper), .wc-order-refund-items, #order_line_items')
+                    .remove();
+
+                targetEl.find('.wc-order-totals-items, #order_shipping_line_items, #order_fee_line_items, #order_refunds, .wc-order-add-item, .wc-order-bulk-actions, script')
+                    .remove();
+
+                /** Add item notes and Die/Laser file */
+                targetEl.find('.wrap_note_item, .stamp_equipment').each( function() {
+                    self.switchTextAreaWithSpan( $(this) );
+                });
+
+                /** Attach styles to cloned element */
+                targetEl.prepend($('#wooexp-style'));
+
+                /** Return original HTML contents after successful printing */
+                $(window).on('afterprint', () => {
+                    location.reload();
+                })
+
+                /** Print order items list only */
+                document.body.classList.add('wooexp-body-covered');
+                try {
+                    this.$body.children().not('#woo-item-list').remove();
+                } catch (error) {
+                    document.querySelector('.mce-panel').remove()
+                }
+
+                window.print();
+            }
+        }
+
+        $(() => {
+            window.exportInstance = new Export()
         });
 
     } )();
